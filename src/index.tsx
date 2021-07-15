4f2060d264b8a95b9a40548f102a4b5722157f83
genesis-shaneW <shane.w@gen-game.com>
2021-07-15 10:00:41 +0800

    Update rng tool tab content,

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 
 import { Header } from './components/Header';
@@ -27,7 +28,7 @@ const App: React.FC = () => (
 			<div className="nav nav-tabs" id="nav-tab" role="tablist">
 				<button className="nav-link active" id="nav-convert-tab" data-bs-toggle="tab" data-bs-target="#nav-convert" type="button" role="tab" aria-controls="nav-convert" aria-selected="true">Convert</button>
 				<button className="nav-link" id="nav-crypto-tab" data-bs-toggle="tab" data-bs-target="#nav-crypto" type="button" role="tab" aria-controls="nav-crypto" aria-selected="false">Crypto</button>
-				<button className="nav-link" id="nav-rngtool-tab" data-bs-toggle="tab" data-bs-target="#nav-rngtool" type="button" role="tab" aria-controls="nav-rngtool" aria-selected="false">Rng Tool</button>
+				<button className="nav-link" id="nav-rngtool-tab" data-bs-toggle="tab" data-bs-target="#nav-rngtool" type="button" role="tab" aria-controls="nav-rngtool" aria-selected="false">RNG Tool</button>
 			</div>
 		</nav>
 		<div className="tab-content" id="nav-tabContent">
